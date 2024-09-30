05d4bfc5ac78d96d5d3911b7c55fdffb696d4280
Fedor Ivanov <ivnfedor@gmail.com>
2024-09-30 15:36:13 +0300

    Remove commented options

--- flake.nix
+++ flake.nix
@@ -23,23 +23,12 @@
           overlays = [ foundry.overlay ];
         };
 
-        # erlang = pkgs.erlang_26.override {
-        #   version = "26.2.5.1";
-        #   sha256 = "sha256-oxOynBFW++igiJtIv1ZjoMgtmumgnsWgwyVx4buhxCo=";
-        # };
         erlang = pkgs.erlang_27.override {
           version = "27.0";
           sha256 = "sha256-YZWBLcpkm8B4sjoQO7I9ywXcmxXL+Dvq/JYsLsr7TO0=";
         };
         beamPkg = pkgs.beam.packagesWith erlang;
-        # elixir = beamPkg.elixir_1_16;
         elixir = beamPkg.elixir_1_17;
-        
-        # erlang = pkgs.erlang_25.override {
-        #   version = "25.3.2.8";
-        #   sha256 = "sha256-oxOynBFd++igiJtIv1ZjoMgtmumgnsWgwyVx4buhxCo=";
-        # };
-        # elixir = beamPkg.elixir_1_14;
 
         nodejs = pkgs.nodejs-18_x;
 
